ecb41fc36e1e1adfe8a5f2d44a47b906a1abdc9e
Jasynth <74651647+Jasynth@users.noreply.github.com>
2020-11-18 18:19:29 -0500

    Add files via upload

---
+++ Interface.js
@@ -0,0 +1,31 @@
+Content.makeFrontInterface(800, 600);
+
+inline function onButton8Control(component, value)
+{
+	if (value)
+    Engine.openWebsite("https://inreachent.com");
+};
+
+Content.getComponent("Button8").setControlCallback(onButton8Control);
+
+function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
---
+++ PresetData.h
@@ -0,0 +1,28 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_PRESETDATA_H
+#define BINARY_PRESETDATA_H
+
+namespace PresetData
+{
+    extern const char*  externalFiles;
+    const int           externalFilesSize = 51;
+
+    extern const char*  images;
+    const int           imagesSize = 6384429;
+
+    extern const char*  impulses;
+    const int           impulsesSize = 28;
+
+    extern const char*  preset;
+    const int           presetSize = 6915;
+
+    extern const char*  samplemaps;
+    const int           samplemapsSize = 28;
+
+    extern const char*  userPresets;
+    const int           userPresetsSize = 9677;
+
+}
+
+#endif
